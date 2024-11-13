32db7abe4a0c0ad021d07ff0b478785a2aa9355f
Krishna Singh <krisnas9792@gmail.com>
2024-11-13 12:05:55 +0530

    getting url query params for filtering

--- main.py
+++ main.py
@@ -12,15 +12,28 @@ BANDS = [
     ]},
     {"id": 4, "name": "Led Zeppelin", "genre": "Hard Rock"},
     {"id": 5, "name": "The Rolling Stones", "genre": "Rock"},
-    {"id": 6, "name": "Nirvana", "genre": "Grunge"},
+    {"id": 6, "name": "Nirvana", "genre": "Grunge", "albums": [{
+            "title": 'Mater of Reality', "release_date": '1971-11-11' 
+        }
+    ]},
 ]
+
+@app.get("/")
+async def start():
+    return "Application Started"
 
 
 @app.get("/bands")
-async def bands() -> list[Band]:
-    return [
-        Band(**b) for b in BANDS
-    ] 
+async def bands(genre: GenreURLChoices | None = None, has_album: bool = False) -> list[Band]:
+    band_list = [Band(**b) for b in BANDS]
+    if genre:
+        band_list = [
+            b for b in band_list if b.genre.lower() == genre.value
+        ]
+    if has_album:
+        band_list = [b for b in band_list if len(b.albums) > 0]
+    else:
+        return band_list
 
 
 @app.get("/bands/{band_id}")
